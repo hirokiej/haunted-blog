7a2d51ab6f8291ecfd97af6c3c15569385b66d93
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-03-16 23:03:10 +0900

    blog_paramsで許可するパラメーターを制限し、有料特典の不正設定を防ぎました

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -50,7 +50,11 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    if current_user.premium
+      params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    else
+      params.require(:blog).permit(:title, :content, :secret)
+    end
   end
 
   def correct_user
